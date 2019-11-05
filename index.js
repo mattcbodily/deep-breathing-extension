7b20a529123abd7552d4e22b6c8059a7359c0119
Matthew Bodily <mattcbodily@gmail.com>
2019-11-05 16:54:51 -0700

    repetitions work when hard coded

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
+let repetitions = 3;
 let instruction = 'Get Ready';
 let count = 3;
+
+const repetitionCount = document.getElementById('repetition-count');
 
 const instructionText = document.getElementById('instruction');
 instructionText.innerText = instruction;
@@ -9,14 +12,23 @@ counter.innerText = count;
 
 function handleCount(){
     count--
-    if((instruction === 'Get Ready' && count === 0) || (instruction === 'Breathe Out' && count === 0)){
+    if(instruction === 'Get Ready' && count === 0){
         count = 7;
         instruction = 'Breathe In';
+        instructionText.innerText = instruction;
+    } else if(instruction === 'Breathe Out' && count === 0){
+        repetitions = repetitions - 1;
+        instruction = 'Breathe In';
+        count = 7;
+        repetitionCount.innerText = repetitions;
         instructionText.innerText = instruction;
     } else if(instruction === 'Breathe In' && count === 0){
         count = 3;
         instruction = 'Breathe Out';
         instructionText.innerText = instruction;
+    }
+    if(repetitions === 0){
+        alert('All Done')
     }
     counter.innerText = count;
 }
@@ -24,4 +36,3 @@ function handleCount(){
 function handleTick(){
     setInterval(() => handleCount(), 1000)
 }
-
