df1f3281637b982ddcfa6d7c3691430f3cf8832c
Matthew Bodily <mattcbodily@gmail.com>
2019-11-05 16:36:34 -0700

    added instruction toggling. Animation synced up with breathing instructions

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
-let count = 100;
+let instruction = 'Get Ready';
+let count = 3;
+
+const instructionText = document.getElementById('instruction');
+instructionText.innerText = instruction;
 
 const counter = document.getElementById('breathe-counter');
 counter.innerText = count;
 
 function handleCount(){
     count--
+    if((instruction === 'Get Ready' && count === 0) || (instruction === 'Breathe Out' && count === 0)){
+        count = 7;
+        instruction = 'Breathe In';
+        instructionText.innerText = instruction;
+    } else if(instruction === 'Breathe In' && count === 0){
+        count = 3;
+        instruction = 'Breathe Out';
+        instructionText.innerText = instruction;
+    }
     counter.innerText = count;
 }
 
