2d62b76f40a7019d544dc54f686152a4181ceb32
Matthew Bodily <mattcbodily@gmail.com>
2019-11-14 23:09:28 -0700

    added 4-7-8 breathing, and got rid of repetitions.

--- popup.js
+++ popup.js
@@ -1,27 +1,22 @@
-let repetitions = 0;
 let instruction = 'Get Ready';
 let count = 3;
-
-//focus on 7/11 breathing as the technique
 
 const inputContainer = document.getElementById('reps-input-container');
 const counterContainer = document.getElementById('counter-container');
 
-const repetitionCount = document.getElementById('repetition-count');
 const selectInput = document.querySelector('select');
 
 const instructionText = document.getElementById('instruction');
 
 const counter = document.getElementById('breathe-counter');
 
-function handleCount(interval){
+function sevenEleven(){
     count--
     if(instruction === 'Get Ready' && count === 0){
         count = 7;
         instruction = 'Breathe In';
         instructionText.innerText = instruction;
     } else if(instruction === 'Breathe Out' && count === 0){
-        repetitions = repetitions - 1;
         instruction = 'Breathe In';
         count = 7;
         instructionText.innerText = instruction;
@@ -30,28 +25,60 @@ function handleCount(interval){
         instruction = 'Breathe Out';
         instructionText.innerText = instruction;
     }
-    if(repetitions === 0){
-        counterContainer.style = 'display: none;'
-        inputContainer.style = 'display: initial;'
-        clearInterval(interval)
+    // if(repetitions === 0){
+    //     counterContainer.style = 'display: none;'
+    //     inputContainer.style = 'display: initial;'
+    //     clearInterval(interval)
+    // }
+    counter.innerText = count;
+}
+
+function fourSevenEight(){
+    count--
+    console.log(count)
+    if(instruction === 'Get Ready' && count === 0){
+        count = 4;
+        instruction = 'Breathe In';
+        instructionText.innerText = instruction;
+    } else if(instruction === 'Breathe Out' && count === 0){
+        count = 4;
+        instruction = 'Breathe In';
+        instructionText.innerText = instruction;
+    } else if(instruction === 'Breathe In' && count === 0){
+        count = 7;
+        instruction = 'Hold Breath';
+        instructionText.innerText = instruction;
+    } else if(instruction === 'Hold Breath' && count === 0){
+        count = 8
+        instruction = 'Breathe Out'
+        instructionText.innerText = instruction;
     }
     counter.innerText = count;
 }
 
+let interval;
 
-function handleTick(){
-    repetitions = +selectInput.value;
+function handleTick(cb){
     inputContainer.style = 'display: none;'
     counterContainer.style = 'display: initial;'
     instructionText.innerText = instruction;
     counter.innerText = count;
 
-    let interval;
-    interval = setInterval(() => handleCount(interval), 1000)
+    interval = setInterval(() => cb(), 1000)
 }
 
-document.getElementById('submit-button').addEventListener('click', function(){
-    handleTick()
+document.getElementById('seven-eleven-button').addEventListener('click', function(){
+    handleTick(sevenEleven)
+})
+
+document.getElementById('four-seven-eight-button').addEventListener('click', function(){
+    handleTick(fourSevenEight)
+})
+
+document.getElementById('all-done-button').addEventListener('click', function(){
+    counterContainer.style = 'display: none;'
+    inputContainer.style = 'display: initial;'
+    clearInterval(interval)
 })
 
 const infoModalBtn = document.getElementById('information-modal-button');
